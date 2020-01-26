9147d4fcd1e13c12ca092913b5da5f236fd88b02
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-01-26 05:43:44 -0800

    Outlined Function
    developed skeleton covering all cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+	//considering fixtures file, deciced to map cases first
+
+	//if key/value pair is function or undefined, skip
+
+	if (typeof obj === "function") {
+		continue;
+	}
+
+	if (typeof obj === undefined) {
+		continue;
+	}
+
+	//if anything outside of array or object, return that value as string directly
+
+	if (typeof obj === null) {
+		return 'null';
+	}
+
+	if (typeof obj === true) {
+		return 'true';
+	}
+
+	if (typeof obj === false) {
+		return 'false';
+	}
+
+	if (typeof obj === "number") {
+		return obj;
+	}
+
+	if (typeof obj === "string") {
+		return '"' + string + '"';
+	}
+
+	if (Array.isArray(obj) === true) {
+	//need recursive code
+	}
+
+	if (typeof obj === "object") {
+		//need recursive code
+	}
+}
