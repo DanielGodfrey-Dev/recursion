53c2323944e7dcb6bfb8644ac26b82775b2a7c60
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-01-27 10:14:00 -0800

    Implemented Function Successfully
    passed specrunner test.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
+//You should use document.body, element.childNodes, and element.classList
+
 var getElementsByClassName = function(className
 ) {
-  // your code here
+
+//we need to look at all the elements of document.body in a function, determine if className is in the their classList,
+//and keep going (call again on childNodes). Then return all the identified elements.
+
+var result = [];
+
+function classTest(node) {
+	if (_.contains(node.classList, className)) {
+		result.push(node);
+	}
+
+	_.each(node.childNodes, function(child) {
+		classTest(child);
+	});
+}
+
+classTest(document.body);
+
+return result;
+
 };
